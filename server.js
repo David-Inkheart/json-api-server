fc7ed7a87787abebb5776aae51500621bf82714c
David-Inkheart <okoliedave1@gmail.com>
2023-03-23 23:22:29 +0100

    set to default

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 const jsonServer = require('json-server')
-//const clone = require('clone')
+const clone = require('clone')
 const data = require('./db.json')
 
-//const isProductionEnv = process.env.NODE_ENV === 'production';
-//const server = jsonServer.create()
+const isProductionEnv = process.env.NODE_ENV === 'production';
+const server = jsonServer.create()
 
 // For mocking the POST request, POST request won't make any changes to the DB in production environment
-//const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
-//    _isFake: isProductionEnv
-// })
-
-//POST requests will make changes to the DB in production environment
-const router = jsonServer.router(isProductionEnv ? 'db.json' : 'db.json')
-const server = jsonServer.create()
+const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
+    _isFake: isProductionEnv
+})
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 
 server.use((req, res, next) => {
     if (req.path !== '/')
-        router.db.setState('db.json')
+        router.db.setState(clone(data))
     next()
 })
 
